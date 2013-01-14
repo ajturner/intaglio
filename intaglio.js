e4a031d270087a2a09d2ab3c126d3694bed29bd1
Andrew Turner <aturner@esri.com>
2013-01-14 15:13:49 -0500

    Handles a hash to specify the provider

--- intaglio.js
+++ intaglio.js
@@ -1,11 +1,12 @@
 var geomaps = [];
 (function(){
-    var Geomap = function(element) {
+    var Geomap = function(element,provider) {
         var me = this;
         me.center = element.getAttribute("data-center").split(",");
         me.zoom = element.getAttribute("data-zoom");
         me.locdiv = element.getAttribute("data-locations");
-        me.map = new mxn.Mapstraction(element.id,'esri');
+        me.provider = provider;
+        me.map = new mxn.Mapstraction(element.id, provider);
     	var latlon = new mxn.LatLonPoint(me.center[1], me.center[0]);
     	me.map.setCenterAndZoom(latlon, me.zoom);   
         me.locations = [];
@@ -17,7 +18,6 @@ var geomaps = [];
             var locs = document.getElementById(this.locdiv);
             var marker_loc;
             for(var cn = locs.children, j=cn.length-1; j>=0; j--) {
-                console.log("cn", cn)
                 marker_loc = JSON.parse(cn[j].getAttribute("data-coordinates"));
                 
                 icon_marker = new mxn.Marker(new mxn.LatLonPoint(marker_loc[1], marker_loc[0]));
@@ -53,23 +53,27 @@ var geomaps = [];
       if(typeof fn === 'function'){ script.onload = fn; }
       head.appendChild(script);
     };
-    function loadProviders(providers, cb) {
+    function loadProviders(provider, cb) {
+        // the proper way - but not working with cascading MXN loads
         // scriptLoader("http://leaflet.cloudmade.com/dist/leaflet.js", function() {
         //     scriptLoader("http://river.local/Projects/mapstraction/mxn/source/mxn.js?(leaflet)", cb)
         // });
-
-        document.write ('<link href="http://serverapi.arcgisonline.com/jsapi/arcgis/3.3/js/esri/css/esri.css" media="all" rel="stylesheet" type="text/css" \/>');
-        document.write ('<script src="http://serverapi.arcgisonline.com/jsapi/arcgis/?v=3.3" type="text/javascript"><\/script>');       
-        // document.write('<link href="http://leaflet.cloudmade.com/dist/leaflet.css" media="all" rel="stylesheet" type="text/css" \/>');
-        // document.write('<script src="http://leaflet.cloudmade.com/dist/leaflet.js" type="text/javascript"><\/script>');
+        if(provider == "leaflet") {
+            document.write('<link href="http://leaflet.cloudmade.com/dist/leaflet.css" media="all" rel="stylesheet" type="text/css" \/>');
+            document.write('<script src="http://leaflet.cloudmade.com/dist/leaflet.js" type="text/javascript"><\/script>');     
+        } else {
+            document.write ('<link href="http://serverapi.arcgisonline.com/jsapi/arcgis/3.3/js/esri/css/esri.css" media="all" rel="stylesheet" type="text/css" \/>');
+            document.write ('<script src="http://serverapi.arcgisonline.com/jsapi/arcgis/?v=3.3" type="text/javascript"><\/script>');       
+        }
         
-        document.write ('<script type="text/javascript" charset="utf-8" src="https://raw.github.com/mapstraction/mxn/integration/source/mxn.js?(esri)"></script>');
+        document.write ('<script type="text/javascript" charset="utf-8" src="https://raw.github.com/mapstraction/mxn/integration/source/mxn.js?('+provider+')"></script>');
         setTimeout(cb, 1000); // hack for onload
     }
     var g = getElementsByClassName('chassis');
-    loadProviders(["esri"], function() {
+    var provider = (window.location.hash || "esri").replace(/#/,'');
+    loadProviders(provider, function() {
         for(var i = g.length-1; i >= 0; i--) {
-            geomaps.push(new Geomap(g[i]));
+            geomaps.push(new Geomap(g[i], provider));
         }
         setTimeout(function() {geomaps[0].mapLocations()}, 1200); // another onload hack. it's presentation time.
     });
